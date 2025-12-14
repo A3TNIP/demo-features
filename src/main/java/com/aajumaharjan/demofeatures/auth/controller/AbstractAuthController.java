f49a2dd3faaee9c56893e62f1f7fabec57289b0c
Aaju Maharjan <aaju.maharjan@genesecloudacademy.edu.np>
2025-12-14 13:08:09 +0000

    refactor: rename AuthController to AbstractAuthController and update class definition

--- src/main/java/com/aajumaharjan/demofeatures/auth/controller/AbstractAuthController.java
+++ src/main/java/com/aajumaharjan/demofeatures/auth/controller/AbstractAuthController.java
@@ -8,16 +8,12 @@ import com.aajumaharjan.demofeatures.auth.model.UserEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/auth")
-public class AuthController {
+public abstract class AbstractAuthController {
     private final AuthService authService;
     private final UserService userService;
 
-    public AuthController(AuthService authService, UserService userService) {
+    public AbstractAuthController(AuthService authService, UserService userService) {
         this.authService = authService;
         this.userService = userService;
     }
