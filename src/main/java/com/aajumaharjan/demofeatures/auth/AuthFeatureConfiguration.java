91c45a64f6d7585cc22536740b4d6eff6fb94486
Aaju Maharjan <aaju.maharjan@genesecloudacademy.edu.np>
2026-01-01 20:21:02 +0000

    test remove autorepoconfig - fix import

--- src/main/java/com/aajumaharjan/demofeatures/auth/AuthFeatureConfiguration.java
+++ src/main/java/com/aajumaharjan/demofeatures/auth/AuthFeatureConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan(basePackages = "com.aajumaharjan.demofeatures.auth")
